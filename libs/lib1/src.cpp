b3953c03a640367f6862d54c9636bd26b9525c91
sarangnair1998 <sarangshibu98@gmail.com>
2024-10-07 17:17:31 -0400

    Completed implementation for PID implementation after recieveing from Pair A

--- libs/lib1/src.cpp
+++ libs/lib1/src.cpp
@@ -1,12 +1,30 @@
 #include "lib1.hpp"
 
 double PidController::compute_speed() {
-  // Please write your implementation here
 
-  return get_actual_speed();
+  double error = desired_speed_ - actual_speed_;
+
+  // Proportional term
+  double proportional = kp_ * error;
+
+  // Integral term
+  integral_ += error;
+  double integral = ki_ * integral_;
+
+  // Derivative term
+  double derivative = kd_ * (error - previous_error_);
+
+  // Updating previous error
+  previous_error_ = error;
+
+  // Updating the logic
+  actual_speed_ += proportional + integral + derivative;
+
+  // Return back the original speed
+  return actual_speed_;
 }
 
 double PidController::get_actual_speed() {
-  // Please write your implementation here
+  // The calculated speed
   return actual_speed_;
 }
